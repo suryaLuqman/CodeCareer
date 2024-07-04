751f27164a2d06884c0aa81f8c033c2b2448a647
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-07-04 17:52:15 +0700

    test

--- Backend/routes.js
+++ Backend/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./controllers');
 
-router.get('/', (req, res) => {
+router.get('', (req, res) => {
    res.send('Hello, World!');
 });
 
