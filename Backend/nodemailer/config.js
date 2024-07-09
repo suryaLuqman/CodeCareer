63c1a4b047f91f12b803c7b16d71942c34cf3e70
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-07-10 00:56:46 +0700

    feat condition btn learningpath

--- Backend/nodemailer/config.js
+++ Backend/nodemailer/config.js
@@ -6,13 +6,13 @@ const { initializeApp } = require("firebase/app");
 const { getAnalytics } = require("firebase/analytics");
 const { getStorage, ref, getDownloadURL, uploadBytesResumable, deleteObject } = require("firebase/storage");
 
-console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY);
-console.log("FIREBASE_AUTH_DOMAIN:", process.env.FIREBASE_AUTH_DOMAIN);
-console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
-console.log("FIREBASE_STORAGE_BUCKET:", process.env.FIREBASE_STORAGE_BUCKET);
-console.log("FIREBASE_MESSAGING_SENDER_ID:", process.env.FIREBASE_MESSAGING_SENDER_ID);
-console.log("FIREBASE_APP_ID:", process.env.FIREBASE_APP_ID);
-console.log("FIREBASE_MEASUREMENT_ID:", process.env.FIREBASE_MEASUREMENT_ID);
+// console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY);
+// console.log("FIREBASE_AUTH_DOMAIN:", process.env.FIREBASE_AUTH_DOMAIN);
+// console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
+// console.log("FIREBASE_STORAGE_BUCKET:", process.env.FIREBASE_STORAGE_BUCKET);
+// console.log("FIREBASE_MESSAGING_SENDER_ID:", process.env.FIREBASE_MESSAGING_SENDER_ID);
+// console.log("FIREBASE_APP_ID:", process.env.FIREBASE_APP_ID);
+// console.log("FIREBASE_MEASUREMENT_ID:", process.env.FIREBASE_MEASUREMENT_ID);
 
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_PRIVATE_KEY,
